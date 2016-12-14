2066ac699ec25c7bc782be950bf39b47c13b77d0
ubuntu <helloworldjf@163.com>
2016-12-14 15:39:51 +0800

    update

---
+++ 15/15_3.h
@@ -0,0 +1,34 @@
+#ifndef 15_3_H
+#define 15_3_H
+
+#include <iostream>
+
+
+class Quote
+{
+public:
+	Quote() = default;
+	Quote(const std::string book, double sales_price):
+			bookNo(book),price(sales_price) {}
+	std::string isbn() { return bookNo;}
+	virtual double net_price(std::size_t n) const { return n*price;}
+
+	virtual ~Quote() = default;
+	
+private:
+	std::string bookNo;
+private:
+	double price = 0.0;
+};
+
+double print_total(ostream &os, const Quote &item, size_t n)
+{
+	double ret = item.net_price(n);
+	os << "ISBN:" << item.isbn() << " # sold: " << n << " total due: "
+	   << ret << std::endl;
+	   return ret;
+}
+
+
+
+#endif
